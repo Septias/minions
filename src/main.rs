8a4b39a9118baeaf99cf7c093079dc0e5e39ae4d
Septias <39526136+Septias@users.noreply.github.com>
2020-11-03 22:51:54 +0100

    refactoring - moving state to a seperate file

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,20 @@
 use amethyst::{
-    assets::{PrefabLoader, PrefabLoaderSystemDesc, RonFormat},
-    core::math::Point3,
-    core::{
-        ecs::{Builder, WorldExt},
-        math::Vector3,
-        TransformBundle,
-    },
-    renderer::debug_drawing::DebugLines,
-    renderer::debug_drawing::DebugLinesComponent,
-    renderer::debug_drawing::DebugLinesParams,
-    renderer::palette::Srgba,
-    renderer::RenderSkybox,
+    assets::PrefabLoaderSystemDesc,
+    core::TransformBundle,
     renderer::{
         rendy::mesh::{Normal, Position, TexCoord},
         types::DefaultBackend,
-        RenderDebugLines, RenderShaded3D, RenderToWindow, RenderingBundle,
+        RenderDebugLines, RenderShaded3D, RenderSkybox, RenderToWindow, RenderingBundle,
     },
     utils::application_root_dir,
     utils::scene::BasicScenePrefab,
-    Application, GameData, GameDataBuilder, SimpleState, StateData,
+    Application, GameDataBuilder,
 };
 
-type MyPrefabData = BasicScenePrefab<(Vec<Position>, Vec<Normal>, Vec<TexCoord>)>;
-struct Example;
+pub type MyPrefabData = BasicScenePrefab<(Vec<Position>, Vec<Normal>, Vec<TexCoord>)>;
 
-impl SimpleState for Example {
-    fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
-        let handle = data.world.exec(|loader: PrefabLoader<'_, MyPrefabData>| {
-            loader.load("prefab/sphere.ron", RonFormat, ())
-        });
-        data.world.create_entity().with(handle).build();
-
-        data.world.insert(DebugLines::new());
-        // Configure width of lines. Optional step
-        data.world.insert(DebugLinesParams { line_width: 1.0 });
-
-        // Setup debug lines as a component and add lines to render axes & grid
-        let mut debug_lines_component = DebugLinesComponent::with_capacity(100);
-
-        debug_lines_component.add_direction(
-            Point3::new(0.0, 0.0001, 0.0),
-            Vector3::new(0.2, 0.0, 0.0),
-            Srgba::new(1.0, 0.0, 0.23, 1.0),
-        );
-
-        // Y-axis (yellowish-green)
-        debug_lines_component.add_direction(
-            Point3::new(0.0, 0.0, 0.0),
-            Vector3::new(0.0, 0.2, 0.0),
-            Srgba::new(0.5, 0.85, 0.1, 1.0),
-        );
-
-        // Z-axis (blue)
-        debug_lines_component.add_direction(
-            Point3::new(0.0, 0.0001, 0.0),
-            Vector3::new(0.0, 0.0, 0.2),
-            Srgba::new(0.2, 0.75, 0.93, 1.0),
-        );
-
-        data.world.register::<DebugLinesComponent>();
-        data.world
-            .create_entity()
-            .with(debug_lines_component)
-            .build();
-    }
-}
+mod minions;
+use crate::minions::Minions;
 
 fn main() -> amethyst::Result<()> {
     amethyst::start_logger(Default::default());
@@ -87,7 +37,7 @@ fn main() -> amethyst::Result<()> {
                 .with_plugin(RenderShaded3D::default()),
         )?;
 
-    let mut game = Application::new(assets_dir, Example, game_data)?;
+    let mut game = Application::new(assets_dir, Minions::default(), game_data)?;
     game.run();
     Ok(())
 }
--- src/minions.rs
+++ src/minions.rs
@@ -1,6 +1,51 @@
-use amethyst::prelude::*;
+use amethyst::{
+    assets::{PrefabLoader, RonFormat},
+    core::{
+        ecs::{Builder, WorldExt},
+        math::{Point3, Vector3},
+    },
+    prelude::*,
+    renderer::debug_drawing::{DebugLines, DebugLinesComponent, DebugLinesParams},
+    renderer::palette::Srgba,
+    SimpleState,
+};
+
+use crate::MyPrefabData;
 
 #[derive(Default)]
 pub struct Minions {}
 
-impl SimpleState for Minions {}
+impl SimpleState for Minions {
+    fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+        let handle = data.world.exec(|loader: PrefabLoader<'_, MyPrefabData>| {
+            loader.load("prefab/sphere.ron", RonFormat, ())
+        });
+        data.world.create_entity().with(handle).build();
+        initialize_debug_lines(data.world);
+    }
+}
+
+fn initialize_debug_lines(world: &mut World) {
+    world.insert(DebugLines::new());
+    world.insert(DebugLinesParams { line_width: 1.0 });
+    let mut debug_lines_component = DebugLinesComponent::with_capacity(3);
+
+    debug_lines_component.add_direction(
+        Point3::new(0.0, 0.0001, 0.0),
+        Vector3::new(0.2, 0.0, 0.0),
+        Srgba::new(1.0, 0.0, 0.23, 1.0),
+    );
+    debug_lines_component.add_direction(
+        Point3::new(0.0, 0.0, 0.0),
+        Vector3::new(0.0, 0.2, 0.0),
+        Srgba::new(0.5, 0.85, 0.1, 1.0),
+    );
+    debug_lines_component.add_direction(
+        Point3::new(0.0, 0.0001, 0.0),
+        Vector3::new(0.0, 0.0, 0.2),
+        Srgba::new(0.2, 0.75, 0.93, 1.0),
+    );
+
+    world.register::<DebugLinesComponent>();
+    world.create_entity().with(debug_lines_component).build();
+}
