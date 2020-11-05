d97c8a1b49b88041d433761c2620d0d4b99eb87d
Septias <39526136+Septias@users.noreply.github.com>
2020-11-05 17:54:57 +0100

    move plane out of prefab and to code

--- src/minions.rs
+++ src/minions.rs
@@ -1,12 +1,19 @@
 use amethyst::{
-    assets::{PrefabLoader, RonFormat},
+    assets::{AssetLoaderSystemData, Handle, PrefabLoader, RonFormat},
     core::{
         ecs::{Builder, WorldExt},
         math::{Point3, Vector3},
+        Transform,
     },
     prelude::*,
-    renderer::debug_drawing::{DebugLines, DebugLinesComponent, DebugLinesParams},
-    renderer::palette::Srgba,
+    renderer::{
+        debug_drawing::{DebugLines, DebugLinesComponent, DebugLinesParams},
+        loaders::load_from_linear_rgba,
+        palette::{LinSrgba, Srgba},
+        rendy::mesh::{Normal, Position, Tangent, TexCoord},
+        shape::Shape,
+        Material, MaterialDefaults, Mesh, Texture,
+    },
     SimpleState,
 };
 
@@ -22,6 +29,7 @@ impl SimpleState for Minions {
         });
         data.world.create_entity().with(handle).build();
         initialize_debug_lines(data.world);
+        initialize_ground(data.world);
     }
 }
 
@@ -48,4 +56,64 @@ fn initialize_debug_lines(world: &mut World) {
 
     world.register::<DebugLinesComponent>();
     world.create_entity().with(debug_lines_component).build();
+}
+
+fn initialize_ground(world: &mut World) {
+    let (width, height) = (5, 5);
+
+    let mat_defaults = world.read_resource::<MaterialDefaults>().0.clone();
+    let mesh = create_mesh(world);
+    let albedo = create_albedo(world);
+    let roughness = 1.0f32;
+    let metallic = 1.0f32;
+
+    let mtl = world.exec(
+        |(mtl_loader, tex_loader): (
+            AssetLoaderSystemData<'_, Material>,
+            AssetLoaderSystemData<'_, Texture>,
+        )| {
+            let metallic_roughness = tex_loader.load_from_data(
+                load_from_linear_rgba(LinSrgba::new(0.0, roughness, metallic, 0.0)).into(),
+                (),
+            );
+
+            mtl_loader.load_from_data(
+                Material {
+                    albedo: albedo.clone(),
+                    metallic_roughness,
+                    ..mat_defaults.clone()
+                },
+                (),
+            )
+        },
+    );
+
+    let mut pos = Transform::default();
+    pos.append_rotation_x_axis(-1.5707);
+    world.create_entity().with(mesh).with(mtl).with(pos).build();
+    /* for _ in 0..width {
+        for _ in 0..height {
+            world.create_entity().build();
+        }
+    } */
+}
+
+fn create_mesh(world: &mut World) -> Handle<Mesh> {
+    world.exec(|loader: AssetLoaderSystemData<'_, Mesh>| {
+        loader.load_from_data(
+            Shape::Plane(None)
+                .generate::<(Vec<Position>, Vec<Normal>, Vec<Tangent>, Vec<TexCoord>)>(None)
+                .into(),
+            (),
+        )
+    })
+}
+
+fn create_albedo(world: &mut World) -> Handle<Texture> {
+    world.exec(|loader: AssetLoaderSystemData<'_, Texture>| {
+        loader.load_from_data(
+            load_from_linear_rgba(LinSrgba::new(1.0, 1.0, 1.0, 0.5)).into(),
+            (),
+        )
+    })
 }
